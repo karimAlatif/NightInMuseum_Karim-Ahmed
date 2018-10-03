7becbf4fa39df6a369d0263e0692559c79e8009c
KarimAlatif <themoonisnice@hotmail.com>
2018-10-03 04:43:41 +0200

    Create

---
+++ Assets/Scripts/EntranceScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntranceScript : MonoBehaviour {
+
+
+	public int RoomIndex;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   
+	private void OnTriggerEnter(Collider col){
+		if(col.gameObject.name=="Player")
+		GameManager.Instance.TurnOnCinemaRoom (RoomIndex);
+	}
+
+	private void OnTriggerExit(Collider col){
+		
+		if(col.gameObject.name=="Player")
+		GameManager.Instance.TurnOffCinemaRoom (RoomIndex);
+
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	private static GameManager instance;
+	public static GameManager Instance{ get { return instance; } }  //Make Instance to this game script so i can call it from any othe script
+
+
+	public List<GameObject> AllVideoPlayer= new List<GameObject>();
+	public GameObject SceneLight;
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TurnOnCinemaRoom(int RoomIndex){
+
+		AllVideoPlayer [RoomIndex].gameObject.SetActive (true); //Turn on room video
+		SceneLight.gameObject.SetActive (false); //turn off the light
+
+	}
+
+	public void TurnOffCinemaRoom(int RoomIndex){
+
+		AllVideoPlayer [RoomIndex].gameObject.SetActive (false); //Turn off room video
+		SceneLight.gameObject.SetActive (true); //turn on the light
+
+	}
+}
